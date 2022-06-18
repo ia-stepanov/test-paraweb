f10750ea635cb256859e87ace43172a0d1ad39ee
Igor Stepanov <igorstepanov@hotmail.com>
2022-06-18 23:34:38 +0300

    build(тестовое задание): собрать проект webpack'ом

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
+// import "../node_modules/jquery/dist/jquery.js";
+// import "../node_modules/slick-carousel/slick/slick.js";
+
+import './index.css';
 import $ from "jquery";
 import "slick-carousel";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,65 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: { main: "./pages/script.js" },
+  entry: {
+    main: "./src/pages/index.js",
+  },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./dist"),
     filename: "main.js",
     publicPath: "",
   },
-  // watch: true,
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
 };
